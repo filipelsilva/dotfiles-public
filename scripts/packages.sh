886df774dc39d7b71e614da3ba91eadd9f7ce34a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-13 17:44:21 +0000

    tmux keybind change, installer changes, now pip installs from python and not from package manager

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,17 +1,79 @@
 #!/usr/bin/env bash
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm zsh git tk curl wget glances htop neovim python-pip\
-    python python-setuptools pypy gcc valgrind gdb unzip zip tmux lynx tree\
-    ripgrep fd the_silver_searcher neofetch xclip pkg-config go make entr ctags\
-    rlwrap hexyl bat cmake shellcheck hyperfine onefetch npm atool ascii nodejs\
-    pacman-contrib zsh-completions zoxide jq xcape
+sudo pacman -S --noconfirm \
+    zsh \
+    git \
+    tk \
+    curl \
+    wget \
+    glances \
+    htop \
+    neovim \
+    python \
+    pypy \
+    gcc \
+    valgrind \
+    gdb \
+    unzip \
+    zip \
+    tmux \
+    lynx \
+    tree \
+    ripgrep \
+    fd \
+    the_silver_searcher \
+    neofetch \
+    xclip \
+    pkg-config \
+    go \
+    make \
+    entr \
+    ctags \
+    rlwrap \
+    hexyl \
+    bat \
+    cmake \
+    shellcheck \
+    hyperfine \
+    onefetch \
+    npm \
+    atool \
+    ascii \
+    nodejs \
+    pacman-contrib \
+    zsh-completions \
+    zoxide \
+    jq \
+    xcape
 
 # For desktop installation:
+
 if [[ $1 = "full" ]]; then
-    sudo pacman -S --noconfirm alacritty i3 i3status i3lock maim rofi feh arandr\
-        nomacs pavucontrol xdotool arc-gtk-theme lxappearance thunar autorandr\
-        thunar-archive-plugin file-roller okular xss-lock\
-        ttc-iosevka ttc-iosevka-ss12 redshift playerctl brightnessctl
+    sudo pacman -S --noconfirm \
+        alacritty \
+        i3 \
+        i3status \
+        i3lock \
+        maim \
+        rofi \
+        feh \
+        arandr \
+        nomacs \
+        pavucontrol \
+        xdotool \
+        arc-gtk-theme \
+        lxappearance \
+        thunar \
+        autorandr \
+        thunar-archive-plugin \
+        file-roller \
+        okular \
+        xss-lock \
+        redshift \
+        playerctl \
+        brightnessctl \
+        ttc-iosevka \
+        ttc-iosevka-ss12
 fi
 
--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# Install pip
+python3 -m ensurepip --upgrade
+export PATH=$PATH:$HOME/.local/bin
+
 # Pip programs
 pip3 install --user --upgrade pip lizard bpython internetarchive gdbgui tmuxp
 
 # Pip packages (same as programs but are not used as standalone programs)
-pip3 install --user --upgrade pynvim capstone pyelftools 
+pip3 install --user --upgrade pynvim capstone pyelftools pycparser setuptools
 
